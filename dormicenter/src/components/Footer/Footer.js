2e2ea4176a1ac22ac6efc1b4ac5ece73b0d6f43e
Jonatan Ferrari <jonaaf32@gmail.com>
2023-03-21 11:19:11 -0300

    Prueba de cambios en Footer.js

---
+++ dormicenter/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>Este es el footer</h1>
+        <h1>Somos unos cracks!</h1>
+    </div>
+  )
+}
+
+export default Footer
